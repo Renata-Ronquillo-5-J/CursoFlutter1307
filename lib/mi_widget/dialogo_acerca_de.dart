c177956329671811b8e0d2de2afdc1663e3ca99b
RENATA_RONQUILLO_LOPEZ <a.22308051281307@cbtis128.edu.mx>
2025-03-26 19:27:25 +0000

    Ejemplo de dialogo de alerta

---
+++ lib/mi_widget/dialogo_acerca_de.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DialogoAcercaDe extends StatefulWidget {
+  const DialogoAcercaDe({Key? key}) : super(key: key);
+
+  @override
+  State<DialogoAcercaDe> createState() => _DialogoAcercaDeState();
+}
+
+class _DialogoAcercaDeState extends State<DialogoAcercaDe> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Dialogo Acerca deg'),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) => const AboutDialog(
+              applicationIcon: FlutterLogo(),
+              applicationLegalese: 'Legalese',
+              applicationName: 'Flutter App',
+              applicationVersion: '1307',
+              children: [
+                Text('Este texto es creado por alerta'),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ z_recursos/leeme.md
@@ -0,0 +1,5 @@
+## Ejemplo card
+![La tarjeta](Card.png)
+## Ejemplo un Dialogo de alerta
+![La tarjeta](dialogo.png)
+![La tarjeta](dialogor.png)
